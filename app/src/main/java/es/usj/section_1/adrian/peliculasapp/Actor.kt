aa508004f1fa09dea1399ffa1ef1e9f25597522e
AdrianSanRoque <60479266+AdrianSanRoque@users.noreply.github.com>
2021-08-30 19:48:08 +0200

    Delete app directory

--- app/src/main/java/es/usj/section_1/adrian/peliculasapp/Actor.kt
+++
@@ -1,9 +0,0 @@
-package com.example.aplicacionpeliculas
-
-class Actor {
-    var id = 0
-    var name: String? = null
-    override fun toString(): String {
-        return name!!
-    }
-}
--- app/src/main/java/es/usj/section_1/adrian/peliculasapp/_Config.kt
+++
@@ -1,8 +0,0 @@
-package com.example.aplicacionpeliculas
-
-
-object _Config {
-    var ENDPOINT = "http://10.0.2.2" // http://10.0.2.2 for local XAMPP server
-    var USER = "user123"
-    var PASSWORD = "pass123"
-}
